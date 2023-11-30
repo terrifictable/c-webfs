c8c914cc16822d5c12f4e6ac5e927b00c3ea2a12
TerrificTable <omegagaming189@gmail.com>
2023-11-30 14:09:05 +0100

    handle post to '/exit' to shutdown executable & handle get request in own function

--- src/net/client.c
+++ src/net/client.c
@@ -1,13 +1,8 @@
 #include "client.h"
 
-const char* _404 = "HTTP/1.0 404 Not Found\r\n\r\n404 Not Found";
+#define nstr(s) string_news(s)
 
-void send_404(socket_info* si) {
-    int send_result = send(si->sock, _404, strlen(_404), 0);
-    if (send_result == SOCKET_ERROR) {
-        err("[%d] > failed to send '%s', error: %d", si->id, _404, WSAGetLastError());
-    }
-}
+void get_request(socket_info* si, char* req_buf, int req_buf_len);
 
 void get_mode_str(string* str, struct stat* fstat) {
     string_appends(str, (S_ISDIR(fstat->st_mode)) ? "d" : "-");
@@ -33,71 +28,38 @@ extern int should_exit;
 void handle_client(socket_info* si) {
     const int buf_len = 1024;
     char buf[buf_len];
-    int recv_result, send_result;
+    int recv_result;
 
     recv_result = recv(si->sock, buf, buf_len, 0);
     if (recv_result > 0) {
         sock_info("[%d] > bytes received: %d", si->id, recv_result);
 
-        if (strncmp(buf, "GET", 3) != 0) {
-            warn("[%d] > request is not a GET request", si->id);
-            // warn("[%d] > buf: %s", si->id, buf);
-            // TODO: maybe handle POST requests and others
-            send_404(si);
-            goto cleanup;
-        }
+        if (strncmp(buf, "GET", 3) == 0) {
+            get_request(si, buf, buf_len);
+        } else if (strncmp(buf, "POST", 4) == 0) {
+            char* f = buf + 6;
+            *strchr(f, ' ') = 0;
+            char* p = strchr(f, '?');
+            if (p != NULL) {
+                *p = 0; // remove query string
+            }
+            
+            header headers[] = {
+                {nstr("Content-Type"), nstr("text/plain")},
+                {nstr("Server"), nstr("c-webfs")}
+            };
 
 
-        // GET /file.html ...
-        char* f = buf + 5;
-        *strchr(f, ' ') = 0;
-        char* p = strchr(f, '?');
-        if (p != NULL) {
-            *p = 0; // remove query string
-        }
- 
-        char last_c = *(f+strlen(f)-1);
-        if (last_c == '/') {
-            handle_directory(si, f);
-            goto cleanup;
-        }
+            if (strcmp(f, "exit") == 0) {
+                string response = build_response(HTTP_1_0, STATUS_OK, headers, 2, nstr("Exiting..."));
+                send(si->sock, response.str, response.len, 0);
+                should_exit = 1;
+            } else {
+                send_404(si, headers, 2);
+                info("Sent 404");
+            }
 
-        sock_info("[%d] > file requested: %s", si->id, f);
-        FILE* fptr = fopen(f, "rb");
-        if (!fptr) { // NULL
-            warn("[%d] > requested file doesnt exist, sending 404", si->id);
-            send_404(si);
-            goto cleanup;
-        }
-
-        const int fbuf_len = 1024;
-        char* fbuf = calloc(fbuf_len, sizeof(char));
-
-        string fstr = string_new();
-        while (fread(fbuf, sizeof(char), fbuf_len, fptr)) {
-            string_appendf(&fstr, "%s", fbuf);
-            memset(fbuf, 0, fbuf_len);
-        }
-        fclose(fptr);
-        free(fbuf);
-
-
-        header headers[] = {
-            header_new(string_news("Content-Type"), string_news("text/plain")),
-            header_new(string_news("Server"), string_news("c-webfs")),
-        };
-
-        string response = build_response(HTTP_1_0, STATUS_OK, headers, sizeof(headers)/sizeof(*headers), &fstr);
-
-        headers_free(headers, sizeof(headers)/sizeof(*headers));
-
-        send_result = send(si->sock, response.str, response.len, 0);
-        memset(buf, 0, buf_len);
-        string_free(&response);
-        string_free(&fstr);
-
-        if (send_result == SOCKET_ERROR) {
-            err("[%d] > failed to send '%s', error: %d", si->id, buf, WSAGetLastError());
+            headers_free(headers, sizeof(headers)/sizeof(*headers));
             goto cleanup;
         }
     } else if (recv_result == 0 || WSAGetLastError() == 10054) { // magic number: winsock error code for "remote forcfully closed connection"
@@ -126,6 +88,60 @@ client_result_t accept_client(SOCKET sock) {
     return res;
 }
 
+void get_request(socket_info* si, char* req_buf, int req_buf_len) {
+    // GET /file.html ...
+    char* f = req_buf + 5;
+    *strchr(f, ' ') = 0;
+    char* p = strchr(f, '?');
+    if (p != NULL) {
+        *p = 0; // remove query string
+    }
+
+    char last_c = *(f+strlen(f)-1);
+    if (last_c == '/') {
+        handle_directory(si, f);
+        return;
+    }
+
+    sock_info("[%d] > file requested: %s", si->id, f);
+    FILE* fptr = fopen(f, "rb");
+    if (!fptr) { // NULL
+        warn("[%d] > requested file doesnt exist, sending 404", si->id);
+        send_404(si, NULL, 0);
+        return;
+    }
+
+    const int fbuf_len = 1024;
+    char* fbuf = calloc(fbuf_len, sizeof(char));
+
+    string fstr = string_new();
+    while (fread(fbuf, sizeof(char), fbuf_len, fptr)) {
+        string_appendf(&fstr, "%s", fbuf);
+        memset(fbuf, 0, fbuf_len);
+    }
+    fclose(fptr);
+    free(fbuf);
+
+
+    header headers[] = {
+        header_new(nstr("Content-Type"), nstr("text/plain")),
+        header_new(nstr("Server"), nstr("c-webfs")),
+    };
+
+    string response = build_response(HTTP_1_0, STATUS_OK, headers, sizeof(headers)/sizeof(*headers), fstr);
+
+    headers_free(headers, sizeof(headers)/sizeof(*headers));
+
+    int send_result = send(si->sock, response.str, response.len, 0);
+    memset(req_buf, 0, req_buf_len);
+    string_free(&response);
+    string_free(&fstr);
+
+    if (send_result == SOCKET_ERROR) {
+        err("[%d] > failed to send response, error: %d", si->id, WSAGetLastError());
+    }
+}
+
 void handle_directory(socket_info* si, char* f) {
     f--;
 
@@ -134,15 +150,27 @@ void handle_directory(socket_info* si, char* f) {
     string body = string_new(0);
     string_appends(&body, 
         "<head>"
-        "<meta name=\"color-scheme\" content=\"dark\">"
-        "<style>\n"
-        "body {font-family: monospace;}\n"
-        "table td {padding: 0 0.4em;}\n"
-        "a {color:#56c8ff}\n"
-        "a:hover {color:lightblue}\n"
-        "tr:hover td {background-color: #333}\n"
-        "#name{width:200px}\n"
-        "</style>"
+            "<meta name=\"color-scheme\" content=\"dark\">"
+            "<style>"
+                "body {"
+                    "font-family: monospace"
+                "}"
+                "table td {"
+                    "padding: 0 0.4em"
+                "}"
+                "a {"
+                    "color: #56c8ff"
+                "}"
+                "a:hover {"
+                    "color: lightblue"
+                "}"
+                "tr:hover td {"
+                    "background-color: #333"
+                "}"
+                "#name {"
+                    "width: 200px "
+                "}"
+            "</style>"
         "</head>"
         "<body>"
     );
@@ -152,7 +180,7 @@ void handle_directory(socket_info* si, char* f) {
     DIR *d = opendir(dir_name);
     if (!d) {
         err("[%d] > Failed to open dir: %s", si->id, dir_name);
-        send_404(si);
+        send_404(si, NULL, 0);
         goto cleanup;
     }
 
@@ -219,11 +247,11 @@ void handle_directory(socket_info* si, char* f) {
 
 
     header headers[] = {
-        header_new(string_news("Content-Type"), string_news("text/html")),
-        header_new(string_news("Server"), string_news("c-webfs")),
+        {nstr("Content-Type"), nstr("text/html")},
+        {nstr("Server"), nstr("c-webfs")},
     };
 
-    string response = build_response(HTTP_1_0, STATUS_OK, headers, sizeof(headers)/sizeof(*headers), &body);
+    string response = build_response(HTTP_1_0, STATUS_OK, headers, sizeof(headers)/sizeof(*headers), body);
     headers_free(headers, sizeof(headers)/sizeof(*headers));
 
     int send_result = send(si->sock, response.str, response.len, 0);
